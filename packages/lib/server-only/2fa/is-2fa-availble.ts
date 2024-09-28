db24386d01e7562234119ba74607d5e27a8b61bd
Jack <50677723+jtmaveryk@users.noreply.github.com>
2024-09-28 09:15:15 -0400

    refactor: remove DOCUMENSO from ENCRYPTION_KEY variable

--- packages/lib/server-only/2fa/is-2fa-availble.ts
+++ packages/lib/server-only/2fa/is-2fa-availble.ts
@@ -1,6 +1,6 @@
 import type { User } from '@documenso/prisma/client';
 
-import { DOCUMENSO_ENCRYPTION_KEY } from '../../constants/crypto';
+import { ENCRYPTION_KEY } from '../../constants/crypto';
 
 type IsTwoFactorAuthenticationEnabledOptions = {
   user: User;
@@ -9,5 +9,5 @@ type IsTwoFactorAuthenticationEnabledOptions = {
 export const isTwoFactorAuthenticationEnabled = ({
   user,
 }: IsTwoFactorAuthenticationEnabledOptions) => {
-  return user.twoFactorEnabled && typeof DOCUMENSO_ENCRYPTION_KEY === 'string';
+  return user.twoFactorEnabled && typeof ENCRYPTION_KEY === 'string';
 };
